57cc33f71426f89d4a1750041bc9ad08dcb1ee07
Neelabh Paul <npaul_be19@thapar.edu>
2023-11-07 19:04:58 +0530

    another bug fixed

--- server.js
+++ server.js
@@ -15,12 +15,13 @@ const server = require('http').createServer(app)
 const io = require('socket.io')(server, {
     cors: {
         origin: process.env.FRONTEND_URL,
-        methods: ['GET', 'POST']
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
     }
 })
 
 app.use(cors({
     origin: [process.env.FRONTEND_URL],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
 }))
 app.use(cp())
